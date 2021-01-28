12fe02edab435ab0e1eac78f1c719c83853117d4
Alberto <cerrone.alberto93@gmail.com>
2021-01-28 16:44:27 +0100

    working on group_members

--- group_members/views.py
+++ group_members/views.py
@@ -2,8 +2,11 @@ from django.core.exceptions import PermissionDenied
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers.populated import (
+    PopulatedUsersMemberSerializer,
+    PopulatedGroupMemberSerializer,
+)
 
-from .serializers.populated import NestedGroupMemberSerializer, NestedUserSerializer
 from .models import GroupMember
 
 
@@ -12,11 +15,13 @@ class GroupMemberListView(APIView):
 
     def get(self, _request):
         group_members = GroupMember.objects.all()
-        serialized_group_members = NestedUserSerializer(group_members, many=True)
+        serialized_group_members = PopulatedUsersMemberSerializer(
+            group_members, many=True
+        )
         return Response(serialized_group_members.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        group_member_to_create = NestedGroupMemberSerializer(data=request.data)
+        group_member_to_create = PopulatedGroupMemberSerializer(data=request.data)
         if group_member_to_create.is_valid():
             group_member_to_create.save()
             return Response(group_member_to_create.data, status=status.HTTP_201_CREATED)
